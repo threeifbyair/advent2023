8b3deb8bff8413db35e7c2a3d5ffdf616c36a055
Alistair Bell <alistair@bellsonline.com>
2023-12-14 19:19:23 -0500

    Part two was fairly easy!

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -1,8 +1,13 @@
 
 class Day11: AdventDay {
     override func run() {
+        let dilation = argint ?? (partTwo ? 1000000 : 1)
         var answer: Int = 0
         var starMap: [[Bool]] = []
+        var yMap: [Int: Int] = [:]
+        var xMap: [Int: Int] = [:]
+        var realY: Int = 0
+        var effectiveY: Int = 0
         for str in inputStrings {
             if str.length == 0 {
                 continue
@@ -14,11 +19,17 @@ class Day11: AdventDay {
             starMap.append(starLine)
             // Expand the universe here
             if starLine.filter({ $0 }).count == 0 {
-                starMap.append(starLine)
+                effectiveY += dilation
+            } else {
+                yMap[realY] = effectiveY
+                effectiveY += 1
             }
+            realY += 1
         }
         // Now transpose the star map.
         var transposedStarMap: [[Bool]] = []
+        var realX: Int = 0
+        var effectiveX: Int = 0
         for y in 0..<starMap[0].count {
             var mapLine: [Bool] = []
             for x in 0..<starMap.count {
@@ -26,8 +37,12 @@ class Day11: AdventDay {
             }
             transposedStarMap.append(mapLine)
             if mapLine.filter({ $0 }).count == 0 {
-                transposedStarMap.append(mapLine)
+                effectiveX += dilation
+            } else {
+                xMap[realX] = effectiveX
+                effectiveX += 1
             }
+            realX += 1
         }
         // OK, now we have a star map that's been expanded in all directions.
         // Let's find the star coordinates.
@@ -43,7 +58,7 @@ class Day11: AdventDay {
         // every other star.
         for (starId, star) in starCoordinates.enumerated() {
             for otherStar in starCoordinates[starId+1..<starCoordinates.count] {
-                answer += abs(star.0 - otherStar.0) + abs(star.1 - otherStar.1)
+                answer += abs(yMap[star.0]! - yMap[otherStar.0]!) + abs(xMap[star.1]! - xMap[otherStar.1]!)
             }
         }        
         
