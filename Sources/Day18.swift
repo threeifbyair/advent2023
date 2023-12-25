48ea9978d96a51287b9e92b5446d5a69da4fb100
Alistair Bell <alistair@bellsonline.com>
2023-12-25 13:05:18 -0500

    Day 18 part 2.

--- Sources/Day18.swift
+++ Sources/Day18.swift
@@ -1,5 +1,6 @@
 import Foundation
 import RegexBuilder
+import HeapModule
 
 class Trench: Hashable {
     var direction: Direction
@@ -37,123 +38,137 @@ class LagoonMap {
         var x = 0
         var y = 0
         for trench in trenches {
-            var remaining = trench.distance
-            while remaining > 0 {
-                switch trench.direction {
-                case .north:
-                    y += 1
-                case .south:
-                    y -= 1
-                case .east:
-                    x += 1
-                case .west:
-                    x -= 1
-                }
-                remaining -= 1
-                if x < minX {
-                    minX = x
-                }
-                if x > maxX {
-                    maxX = x
-                }
-                if y < minY {
-                    minY = y
-                }
-                if y > maxY {
-                    maxY = y
-                }
-                if trenchMap[y] == nil {
-                    trenchMap[y] = [:]
-                }
-                trenchMap[y]![x] = trench
+            if trenchMap[y] == nil {
+                trenchMap[y] = [:]
+            }
+            trenchMap[y]![x] = trench
+            //print("Trench at \(x), \(y): \(trench.direction) \(trench.distance)")
+            switch trench.direction {
+            case .north:
+                y -= trench.distance
+            case .south:
+                y += trench.distance
+            case .east:
+                x += trench.distance
+            case .west:
+                x -= trench.distance
+            }
+            if x < minX {
+                minX = x
+            }
+            if x > maxX {
+                maxX = x
+            }
+            if y < minY {
+                minY = y
+            }
+            if y > maxY {
+                maxY = y
             }
         }
     }
-
-    func fillInterior() {
-        var y = minY
-        while y <= maxY {
-            if let row = trenchMap[y] {
-                var x = minX
-                var inside = false
-                var onEdge = false
-                var fromSouth = false
-                while x <= maxX {
-                    if onEdge {
-                        if row[x] != nil {
-                            // do nothing, we stay on the edge
-                        }
-                        else {
-                            onEdge = false
-                            // Have we gone outside?
-                            let wentSouth: Bool = trenchMap[y + 1] != nil && trenchMap[y + 1]![x-1] != nil
-                            let stayedSame: Bool = (wentSouth && fromSouth) || (!wentSouth && !fromSouth)
-                            //print("x: \(x) y: \(y) wentSouth: \(wentSouth), fromSouth: \(fromSouth), stayedSame: \(stayedSame), inside: \(inside)")
-
-                            if stayedSame {
-                                // Yeah, this looks wrong, but we switched earlier, so we're good.
-                                inside = !inside
-                            }
-                            if inside {
-                                trenchMap[y]![x] = LagoonMap.openSpace
-                            }
-                        }
-                    } else if inside {
-                        if row[x] == nil {
-                            trenchMap[y]![x] = LagoonMap.openSpace
-                        } else {
-                            inside = false
-                            if row[x+1] != nil {
-                                onEdge = true
-                                fromSouth = trenchMap[y + 1] != nil && trenchMap[y + 1]![x] != nil
-                                //print("x: \(x) y: \(y) frominside mapsouth \(trenchMap[y+1]![x]) fromSouth: \(fromSouth), inside: \(inside)")
-                            }
-                        }
-                    } else {
-                        if row[x] != nil {
-                            inside = true
-                            if row[x+1] != nil {
-                                onEdge = true
-                                fromSouth = trenchMap[y + 1] != nil && trenchMap[y + 1]![x] != nil
-                                //print("x: \(x) y: \(y) fromoutside mapsouth \(trenchMap[y+1]![x]) fromSouth: \(fromSouth), inside: \(inside)")
-                            }
-                        }
-                    }
-                    x += 1
-                }
-            }
-            y += 1
-        }
-    }
-
     
     func howFilled() -> Int {
         var filled = 0
-        for x in minX...maxX {
-            for y in minY...maxY {
-                if trenchMap[y] != nil && trenchMap[y]![x] != nil {
-                    filled += 1
-                }
+        var trenchStatus: Heap<Int> = Heap<Int>()
+        var lastY: Int? = nil
+        for y in trenchMap.keys.sorted() {
+            var ourTrenches: [Int] = []
+            while !trenchStatus.isEmpty {
+                let trench = trenchStatus.popMin()!
+                ourTrenches.append(trench)
             }
+            trenchStatus = Heap<Int>()
+            if lastY != nil && y - lastY! > 0 {
+                let diff = y - lastY!
+                var lineFilled = 0
+                var offset = 0
+                while offset < ourTrenches.count  {
+                    let start = ourTrenches[offset]
+                    let end = ourTrenches[offset + 1]
+                    offset += 2
+                    lineFilled += end - start + 1
+                }
+                //print("\nIntermediate \(lastY!) - \(y), status \(ourTrenches), filling \(lineFilled), \(diff) times")
+                filled += lineFilled * diff
+            }
+            var inside: Bool = false
+            var offset = 0
+            var prevEnd: Int? = nil
+            let keys = trenchMap[y]!.keys.sorted()
+            var lineFilled = 0
+            //print("\nEdge line \(y), status \(ourTrenches), keys \(keys)")
+            while offset <= trenchMap[y]!.count {
+                let edgeStart = (offset == trenchMap[y]!.count ? Int.max :keys[offset])
+                // We may have a trench just coming past.
+                while !ourTrenches.isEmpty && ourTrenches[0] < edgeStart {
+                    let passed = ourTrenches.removeFirst()
+                    if inside {
+                        //print("In passing code, filling \(passed - prevEnd! - 1) for \(prevEnd!) - \(passed)")
+                        lineFilled += passed - prevEnd! - 1
+                    }
+                    //print("In passing code, filling 1 for \(passed)")
+                    lineFilled += 1 // for the trench
+                    inside = !inside
+                    trenchStatus.insert(passed)
+                    //print("Status: in passing, inserting \(passed)")
+                    prevEnd = passed
+                }
+                if offset == trenchMap[y]!.count {
+                    break
+                }
+                let edgeEnd = keys[offset + 1]
+                offset += 2
+                if inside && prevEnd != nil && edgeStart - prevEnd! > 1 {
+                    // Fill this part.
+                    //print("Inside part: Filling \(edgeStart - prevEnd! - 1) for \(prevEnd!) - \(edgeStart)")
+                    lineFilled += edgeStart - prevEnd! - 1
+                }
+                //print("Edge: Filling \(edgeEnd - edgeStart + 1) for \(edgeStart) - \(edgeEnd)")
+                lineFilled += edgeEnd - edgeStart + 1
+                let fromNorth = (!ourTrenches.isEmpty && ourTrenches[0] == edgeStart)
+                if fromNorth  {
+                    // So this is a trench coming from the north.
+                    ourTrenches.removeFirst()
+                    let wentNorth = (!ourTrenches.isEmpty && ourTrenches[0] == edgeEnd)
+                    if wentNorth {
+                        // And it's going north, so no change to the inside status.
+                        // And we lose both ends of the trench.
+                        ourTrenches.removeFirst()
+                        //print("Status: north to north, inserting nothing")
+                    }
+                    else {
+                        // And it's going south.
+                        inside = !inside
+                        trenchStatus.insert(edgeEnd)
+                        //print("Status: north to south, inserting \(edgeEnd)")
+                    }
+                }
+                else {
+                    // This is a trench coming from the south.
+                    let wentNorth = (!ourTrenches.isEmpty && ourTrenches[0] == edgeEnd)
+                    if wentNorth {
+                        // And it's going north.
+                        ourTrenches.removeFirst()
+                        inside = !inside
+                        trenchStatus.insert(edgeStart)
+                        //print("Status: south to north, inserting \(edgeStart)")
+                    }
+                    else {
+                        // And it's going south, so no change to the inside status.
+                        // But we gain both ends of the trench.
+                        trenchStatus.insert(edgeStart)
+                        trenchStatus.insert(edgeEnd)
+                        //print("Status: south to south, inserting \(edgeStart) and \(edgeEnd)")
+                    }
+                }
+                prevEnd = edgeEnd
+            }
+            //print("Edge line \(y), new status \(Array(trenchStatus.unordered)), filling \(lineFilled)")
+            filled += lineFilled
+            lastY = y + 1
         }
         return filled
-    }
-
-    func printMap() {
-        var y = minY
-        while y <= maxY {
-            var x = minX
-            while x <= maxX {
-                if trenchMap[y] != nil && trenchMap[y]![x] != nil {
-                    print(trenchMap[y]![x]! == LagoonMap.openSpace ? "O" : "#", terminator: "")
-                } else {
-                    print(".", terminator: "")
-                }
-                x += 1
-            }
-            print("")
-            y += 1
-        }
     }
 }
 
@@ -189,20 +204,22 @@ class Day18: AdventDay {
             let dirchr = match[direction]
             let dist = Int(match[distance])!
             let col = Int(match[color], radix: 16)!
-            let direction: Direction = dirchr == "U" ? .north : dirchr == "D" ? .south : dirchr == "L" ? .east : .west
-            let trench = Trench(direction: direction, distance: dist, color: col)
-            trenches.append(trench)
+            if partTwo {
+                let dist = Int(col / 16)
+                let col = col % 16
+                let direction: Direction = col == 3 ? .north : col == 1 ? .south : col == 0 ? .east : .west
+                let trench = Trench(direction: direction, distance: dist, color: col)
+                trenches.append(trench)
+            }
+            else {
+                let direction: Direction = dirchr == "U" ? .north : dirchr == "D" ? .south : dirchr == "L" ? .west : .east
+                let trench = Trench(direction: direction, distance: dist, color: col)
+                trenches.append(trench)
+            }
         }
         // Build the best route from the end to the beginning.
         let thisMap = LagoonMap(trenches: trenches)
 
-        //thisMap.printMap()
-        
-        thisMap.fillInterior()
-
-        //print("Filled map:")
-        //thisMap.printMap()
-        
         answer = thisMap.howFilled()
         
         
